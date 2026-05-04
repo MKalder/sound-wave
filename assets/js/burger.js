f7b3c3a0b1e43ebda1100372ad07c60d2c35f0ea
Marius Kalder <147047923+MKalder@users.noreply.github.com>
2026-05-04 16:02:22 +0700

    close btn in burger menu

--- assets/js/burger.js
+++ assets/js/burger.js
@@ -1,21 +1,14 @@
 const burger = document.querySelector(".burger");
-
 const nav = document.querySelector(".navigation");
+const closeBtn = document.querySelector("#close-btn");
 
-const overlay = document.querySelector(".overlay");
 
 burger.addEventListener("click", () => {
-
     nav.classList.toggle("active");
-
     overlay.classList.toggle("active");
-
 });
 
-overlay.addEventListener("click", () => {
-
+closeBtn.addEventListener("click", () => {
     nav.classList.remove("active");
-
     overlay.classList.remove("active");
-
 });
